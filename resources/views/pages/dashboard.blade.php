2cca1446587eb04abe327351ed680ba1f934776c
Damian Ułan <damian.ulan@protonmail.com>
2023-05-24 20:03:01 +0200

    apex charts init

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -3,6 +3,28 @@
 
 @include('layouts.components.alerts')
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row course-cards">
+                <div class="col-md-3">
+                    <div class="card course-card">
+                        <img src=""
+                        <div class="card-body">
+                            <div class="card-title">
+
+                            </div>
+                            <div class="card-text">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
 @section('page-scripts')
 @endsection
