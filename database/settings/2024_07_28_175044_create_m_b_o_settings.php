b2c88daa938b41c5dc31974ac1398f1d170212f0
Damian Ułan <damian.ulan@protonmail.com>
2025-05-10 13:17:39 +0200

    mbo settings update

--- database/settings/2024_07_28_175044_create_m_b_o_settings.php
+++ database/settings/2024_07_28_175044_create_m_b_o_settings.php
@@ -7,6 +7,8 @@ return new class extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('mbo.enabled', true);
-
+        $this->migrator->add('mbo.campaigns_enabled', true);
+        $this->migrator->add('mbo.campaigns_manual', true);
+        $this->migrator->add('mbo.rewards', true);
     }
 };
