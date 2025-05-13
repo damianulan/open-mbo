e6ecc6fdc0e5f72fd057b19a5d2e05e934c204a1
Damian Ułan <damian.ulan@protonmail.com>
2025-05-13 22:25:50 +0200

    webpack update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,18 @@
 // webpack.mix.js
 
-require("dotenv").config();
 let mix = require("laravel-mix");
+require("laravel-mix-string-replace");
+require("dotenv").config();
+
+const locale = process.env.APP_LOCALE;
+const replace_js = {
+    test: /\.js$/,
+    loader: "string-replace-loader",
+    options: {
+        search: "ENV_LOCALE",
+        replace: JSON.stringify(locale),
+    },
+};
 
 mix.webpackConfig((webpack) => {
     return {
@@ -16,11 +27,13 @@ mix.copyDirectory("resources/scripts", "public/scripts");
 
 // base-theme
 mix.js("resources/themes/js/app.js", "public/themes/js")
-    .sass(
-        "resources/themes/light/green.scss",
-        "public/themes/light-green/app.css"
-    )
+    .stringReplace(replace_js)
     .version();
+
+mix.sass(
+    "resources/themes/light/green.scss",
+    "public/themes/light-green/app.css"
+).version();
 
 mix.sass(
     "resources/themes/light/blue.scss",
