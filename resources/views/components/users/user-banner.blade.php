ff74e15e16823ea4bad8ce17ab0d567fb3176270
Damian Ułan <damian.ulan@protonmail.com>
2025-05-25 16:38:54 +0200

    typo fix

--- resources/views/components/users/user-banner.blade.php
+++ resources/views/components/users/user-banner.blade.php
@@ -5,6 +5,7 @@
     <div class="mx-2">
         <div class="fw-bold fs-5">{{ $user->name() }}</div>
         <div class="">{{ $user->email }}</div>
+        <div class="text-muted">{{ $user->getRolesNames()->implode(' | ') }}</div>
     </div>
 
 </a>
