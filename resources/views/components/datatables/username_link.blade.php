c722ed01969cc121d4f2ab700a090dfc1aac1f2f
Damian Ułan <damian.ulan@protonmail.com>
2025-06-15 13:34:50 +0200

    User name error fix

--- resources/views/components/datatables/username_link.blade.php
+++ resources/views/components/datatables/username_link.blade.php
@@ -1,4 +1,4 @@
 <a class="user-details" href="{{ route('users.show', $data->id) }}">
     {!! $data->getAvatarView(30) !!}
-    <span class="ms-2">{{ $data->name() }}</span>
+    <span class="ms-2">{{ $data->name }}</span>
 </a>
