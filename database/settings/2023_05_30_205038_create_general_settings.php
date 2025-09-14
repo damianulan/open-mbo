81cdb9180585450e6b4fd78bc6265eae7f7fc67a
Damian Ułan <damian.ulan@protonmail.com>
2025-09-14 22:52:07 +0200

    App upgrade fix

--- database/settings/2023_05_30_205038_create_general_settings.php
+++ database/settings/2023_05_30_205038_create_general_settings.php
@@ -15,8 +15,8 @@ return new class extends SettingsMigration
         $this->migrator->add('general.debugbar', config('app.env') === 'development' ? true : false);
         $this->migrator->add('general.locale', config('app.locale'));
         $this->migrator->add('general.build', date('YmdHi'));
-        $this->migrator->add('general.release', '0.0.1-beta');
-        $this->migrator->add('general.target_release', 'stable');
+        $this->migrator->add('general.release', config('app.env') === 'production' ? 'stable' : 'dev');
+        $this->migrator->add('general.target_release', config('app.env') === 'production' ? 'stable' : 'dev');
 
         $this->migrator->add('general.date_format', 'd.m.Y');
         $this->migrator->add('general.time_format', 'H:i');
