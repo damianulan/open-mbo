a2d3774399b2dfb311dd2254355dd4e5c41d5142
damianulan <31820473+damianulan@users.noreply.github.com>
2025-10-07 21:25:15 +0000

    Apply automatic changes

--- app/Models/Core/User.php
+++ app/Models/Core/User.php
@@ -21,6 +21,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -33,7 +34,6 @@ use Lucent\Support\Traits\CascadeDeletes;
 use Lucent\Support\Traits\UUID;
 use Lucent\Support\Traits\VirginModel;
 use Sentinel\Traits\HasRolesAndPermissions;
-use Illuminate\Support\Collection;
 
 /**
  * @property string $id
