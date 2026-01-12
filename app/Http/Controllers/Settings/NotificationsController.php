249baf48d7af70d7f4c7768de0a2afddfdcc9535
Damian Ułan <damian.ulan@protonmail.com>
2026-01-12 17:07:53 +0100

    Coding Logic fix

--- app/Http/Controllers/Settings/NotificationsController.php
+++ app/Http/Controllers/Settings/NotificationsController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Settings;
 
 use App\DataTables\Settings\NotificationsDataTable;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\JsonResponse;
 
 class NotificationsController extends SettingsController
 {
     /**
      * Show the application dashboard.
      */
-    public function index(NotificationsDataTable $dataTable): Renderable
+    public function index(NotificationsDataTable $dataTable): Renderable|JsonResponse
     {
         return $dataTable->render('pages.settings.notifications.index', array(
             'table' => $dataTable,
