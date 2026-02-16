6facb396d8dbf1080470b80078f31ee2333d5169
Damian Ułan <damian.ulan@protonmail.com>
2026-02-16 11:31:24 +0100

    config laraverse

---
+++ config/laraverse.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laraverse Configuration file v1.0
+    |--------------------------------------------------------------------------
+    |
+    | These are package's default configuration options.
+    |
+    */
+    'sources' => [
+        // slug => path
+        'docs' => 'docs',
+        'formforge' => 'vendor/damianulan/laravel-form-forge/docs',
+    ],
+
+];
