868ba735fd6f09a49cdf1cffaac1751a0a89c9fd
Damian Ułan <damian.ulan@protonmail.com>
2026-02-17 13:41:54 +0100

    form fix

--- app/Forms/Settings/UsersForm.php
+++ app/Forms/Settings/UsersForm.php
@@ -7,6 +7,7 @@ use App\Forms\Traits\SettingsForm;
 use FormForge\Base\Form;
 use FormForge\Base\FormComponent;
 use FormForge\FormBuilder;
+use FormForge\Components\Dictionary;
 
 class UsersForm extends Form
 {
@@ -27,7 +28,7 @@ class UsersForm extends Form
                 __('forms.settings.general.auth'),
                 fn (FormBuilder $builder) => $builder
                     ->add(FormComponent::select('auth_identifier', $this->auth_identifier, Dictionary::fromEnum(AuthIdentifier::class))
-                        ->label(__('forms.settings.users.auth_identifier'))->noEmpty()->key(self::settingsKey('users.auth_identifier')))
+                        ->label(__('forms.settings.users.auth_identifier'))->info(__('forms.settings.users.info.auth_identifier'))->noEmpty()->key(self::settingsKey('users.auth_identifier')))
                     ->add(FormComponent::switch('password_change_firstlogin', $this->password_change_firstlogin)->label(__('forms.settings.users.password_change_firstlogin'))->info(__('forms.settings.users.info.password_change_firstlogin'))->key(self::settingsKey('users.password_change_firstlogin')))
                     ->add(FormComponent::switch('force_password_change_reset', $this->force_password_change_reset)->label(__('forms.settings.users.force_password_change_reset'))->info(__('forms.settings.users.info.force_password_change_reset'))->key(self::settingsKey('users.force_password_change_reset')))
                     ->add(FormComponent::numeric('password_min_length', $this->password_min_length)->label(__('forms.settings.users.password_min_length'))->info(__('forms.settings.users.info.password_min_length'))->key(self::settingsKey('users.password_min_length')))
