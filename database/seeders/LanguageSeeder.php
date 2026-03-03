a19599c722eb7b5e9cd1163c865358cfd6258088
Damian Ułan <damian.ulan@protonmail.com>
2026-03-03 21:45:47 +0100

    Migrations rework

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -7,12 +7,10 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use Throwable;
 
 class LanguageSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $files = File::allFiles(base_path('lang/src'));
@@ -20,34 +18,34 @@ class LanguageSeeder extends Seeder
         foreach ($files as $file) {
             $group = Str::of($file->getFilename())->before('.')->toString();
             $contents = $file->getContents();
-            if ( ! empty($contents)) {
-                $contents = json_decode($contents, true);
+            if (empty($contents)) {
+                continue;
+            }
 
-                $contents = Arr::dot($contents);
-                $lines = [];
+            try {
+                $decoded = json_decode($contents, true, 512, JSON_THROW_ON_ERROR);
+            } catch (Throwable) {
+                continue;
+            }
 
-                foreach ($contents as $key => $value) {
-                    $k = Str::beforeLast($key, '.');
-                    $l = Str::afterLast($key, '.');
-                    $lines[$k][$l] = $value;
-                }
+            $lines = [];
+            foreach (Arr::dot($decoded) as $key => $value) {
+                $lineKey = Str::beforeLast($key, '.');
+                $locale = Str::afterLast($key, '.');
 
-                foreach ($lines as $key => $value) {
-                    $instance = LanguageModel::where('group', $group)->where('key', $key)->first();
-                    if ($instance) {
-                        $instance->group = $group;
-                        $instance->key = $key;
-                        $instance->text = $value;
-                        $instance->update();
-                    } else {
-                        LanguageModel::create([
-                            'group' => $group,
-                            'key' => $key,
-                            'text' => $value,
-                        ]);
-                    }
-                }
+                $lines[$lineKey][$locale] = $value;
+            }
 
+            foreach ($lines as $key => $value) {
+                LanguageModel::query()->updateOrCreate(
+                    [
+                        'group' => $group,
+                        'key' => $key,
+                    ],
+                    [
+                        'text' => $value,
+                    ],
+                );
             }
         }
     }
