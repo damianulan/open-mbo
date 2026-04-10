97251382fc05688542373e4e5b213f55b35d072b
Damian Ułan <damian.ulan@protonmail.com>
2026-04-10 21:28:59 +0200

    migration fix

--- database/migrations/2024_04_18_181825_create_activity_log_table.php
+++ database/migrations/2024_04_18_181825_create_activity_log_table.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateActivityLogTable extends Migration
+return new class () extends Migration
 {
     public function up(): void
     {
         Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'), function (Blueprint $table): void {
             $table->bigIncrements('id');
-            $table->string('log_name')->nullable();
+            $table->string('log_name')->nullable()->index();
             $table->text('description');
             $table->nullableMorphs('subject', 'subject');
             $table->nullableMorphs('causer', 'causer');
+            $table->json('attribute_changes')->nullable();
             $table->json('properties')->nullable();
             $table->timestamps();
-            $table->index('log_name');
         });
     }
 
@@ -24,4 +24,4 @@ class CreateActivityLogTable extends Migration
     {
         Schema::connection(config('activitylog.database_connection'))->dropIfExists(config('activitylog.table_name'));
     }
-}
+};
